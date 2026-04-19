af14a547d9f3ece131bf77368fea595e97beb3d9
huang467 <3530681313@qq.com>
2026-04-20 04:01:46 +0800

    Fix: 为 face-api.js 添加 Webpack 兼容性配置，忽略 fs 模块

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,14 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        fs: false,
+        path: false,
+        crypto: false
+      }
+    }
+  }
 })
